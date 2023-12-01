34f31aeb229376f17afa1555b2abb1a461a08580
Aaron Lal <lucidlear@gmail.com>
2023-12-01 13:09:27 +1100

    dynamic routes

---
+++ next-app/app/users/[id]/photos/[photoId]/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Props {
+  params: { id: number; photoId: number };
+}
+
+const UserPhotoDetails = ({ params: { id, photoId } }: Props) => {
+  return (
+    <div>
+      UserPhotoDetails
+      {id} {photoId}
+    </div>
+  );
+};
+
+export default UserPhotoDetails;
