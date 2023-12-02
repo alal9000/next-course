06ca4e33f9f76266a0447c960b9d460f574ff897
Aaron Lal <lucidlear@gmail.com>
2023-12-02 13:10:37 +1100

    ex - building products API

--- next-app/app/api/products/route.tsx
+++ next-app/app/api/products/route.tsx
@@ -4,8 +4,7 @@ import schema from "./schema";
 export function GET(request: NextRequest) {
   return NextResponse.json([
     { id: 1, name: "milk", price: 2.5 },
-    { id: 2, name: "bread", price: 3.5 },
-    { id: 3, name: "eggs", price: 4 }
+    { id: 2, name: "bread", price: 3.5 }
   ]);
 }
 
@@ -14,8 +13,5 @@ export async function POST(request: NextRequest) {
   const validation = schema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
-  return NextResponse.json(
-    { id: 4, name: body.name, price: body.price },
-    { status: 201 }
-  );
+  return NextResponse.json({ id: 4, name: body.name, price: body.price }, { status: 201 });
 }
