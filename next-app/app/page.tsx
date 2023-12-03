9df41594e36bba3f64f138ae432090b8396fe760
Aaron Lal <lucidlear@gmail.com>
2023-12-03 11:06:28 +1100

    accessing session on the server

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,11 +1,15 @@
+import { getServerSession } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 import ProductCard from "./components/ProductCard";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
-      <h1>Hello world</h1>
+      <h1>Hello { session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
     </main>
