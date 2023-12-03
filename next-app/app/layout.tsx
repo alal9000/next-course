aa12d1c1c7b218aa22e325bbe7357a1bdf4cc4c3
Aaron Lal <lucidlear@gmail.com>
2023-12-03 19:22:48 +1100

    using fonts

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -1,11 +1,22 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Roboto } from "next/font/google";
+import localFont from "next/font/local";
 import NavBar from "./NavBar";
 import { SessionProvider } from "next-auth/react";
 import AuthProvider from "./auth/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500"]
+});
+
+const poppins = localFont({
+  src: "../public/fonts/poppins-regular-webfont.woff2",
+  variable: "--font-poppins"
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,10 +30,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="winter">
-      <body className={inter.className}>
+      <body className={poppins.variable}>
         <AuthProvider>
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <h1>Hello World</h1>
+            {children}
+          </main>
         </AuthProvider>
       </body>
     </html>
